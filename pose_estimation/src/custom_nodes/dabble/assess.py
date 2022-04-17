c26207af6fa71044b20136bcb968b2fd486513c0
JoyLinWQ <69743962+JoyLinWQ@users.noreply.github.com>
2022-04-17 19:37:10 +0800

    clean up codes for shoulders and hips only

--- pose_estimation/src/custom_nodes/dabble/assess.py
+++ pose_estimation/src/custom_nodes/dabble/assess.py
@@ -1,5 +1,5 @@
 """
-Node template for creating custom nodes.
+Custom node for assessing good or bad posture.
 """
 
 from typing import Any, List, Tuple, Dict
@@ -15,10 +15,8 @@ BLACK = (51, 0, 0)
 THRESHOLD = 0.1               # ignore keypoints below this threshold
 OFFSET = 0.03                 # offset between hip and shoulder scaled x coordinates
 DIRECTION = "right"           # right-facing checks shoulder>hip, left-facing checks shoulder<hip
-KP_RIGHT_EAR = 4              # PoseNet's skeletal keypoints
-KP_RIGHT_SHOULDER = 6
+KP_RIGHT_SHOULDER = 6         # PoseNet's skeletal keypoints
 KP_RIGHT_HIP = 12
-KP_LEFT_EAR = 3     
 KP_LEFT_SHOULDER = 5
 KP_LEFT_HIP = 11
 
@@ -69,9 +67,6 @@ class Node(AbstractNode):
         super().__init__(config, node_path=__name__, **kwargs)
 
         # setup object working variables
-        self.avg_shoulder = None
-        self.avg_ear = None
-        # self.ear_before_shoulder = None
         self.shoulder_before_hip = None
         self.offset = OFFSET
         self.posture = None
@@ -86,8 +81,6 @@ class Node(AbstractNode):
         Returns:
             outputs (dict): Dictionary with keys "img".
         """
-        # print keypoint labels for right ear and right shoulder
-        # calculate angle between right ear and right shoulder
         # get required inputs from pipeline
         img = inputs["img"]
         keypoints = inputs["keypoints"]
@@ -95,12 +88,11 @@ class Node(AbstractNode):
 
         img_size = (img.shape[1], img.shape[0])  # image width, height
 
+        # print keypoint labels for shoulders and hips
         the_keypoints = keypoints[0]              # image only has one person
         the_keypoint_scores = keypoint_scores[0]  # only one set of scores
-        right_ear = None
         right_shoulder = None
         right_hip = None
-        left_ear = None
         left_shoulder = None
         left_hip = None
 
@@ -112,11 +104,7 @@ class Node(AbstractNode):
                 x, y = map_keypoint_to_image_coords(keypoints.tolist(), img_size)
                 x_y_str = f"({x}, {y})"
 
-                if i == KP_RIGHT_EAR:
-                    right_ear = keypoints
-                    the_color = RED
-                    kp_name_str = "right ear"
-                elif i == KP_RIGHT_SHOULDER:
+                if i == KP_RIGHT_SHOULDER:
                     right_shoulder = keypoints
                     the_color = RED
                     kp_name_str = "right shoulder"
@@ -124,10 +112,6 @@ class Node(AbstractNode):
                     right_hip = keypoints
                     the_color = RED
                     kp_name_str = "right hip"
-                elif i == KP_LEFT_EAR:
-                    left_ear = keypoints
-                    the_color = YELLOW
-                    kp_name_str = "left ear"
                 elif i == KP_LEFT_SHOULDER:
                     left_shoulder = keypoints
                     the_color = YELLOW
@@ -143,36 +127,7 @@ class Node(AbstractNode):
                 # print(i, x_y_str, keypoints, kp_name_str, keypoint_score)
                 img = draw_text(img, x, y, kp_name_str, the_color, 0.7)
 
-        # do calculations
-        # A. ear vs shoulder
-        # B. shoulder vs hip
-        # C. overall
-
-        # A1. choose either right or left ear vs shoulder for comparison
-        # if (left_shoulder is not None and left_ear is not None):
-            # ear = left_ear
-            # shoulder = left_shoulder
-        # elif (right_shoulder is not None and right_ear is not None):
-            # ear = right_ear
-            # shoulder = right_shoulder
-        # else:
-            # if left_shoulder is None and right_shoulder is None:
-                # shoulder = None
-            # elif left_shoulder is None:
-                # shoulder = right_shoulder
-            # elif right_shoulder is None:
-                # shoulder = left_shoulder
-            
-            # if left_ear is None and right_ear is None:
-                # ear = None
-            # elif left_ear is None:
-                # ear = right_ear
-            # elif right_ear is None:
-                # ear = left_ear
-        # print("ear vs shoulder", ear, shoulder)
-
-
-        # B1. shoulder vs hip
+        # assign shoulder and hip
         if (left_shoulder is not None and left_hip is not None):
             hip = left_hip
             shoulder = left_shoulder
@@ -195,52 +150,7 @@ class Node(AbstractNode):
                 hip = left_hip
         # print("shoulder vs hip", shoulder, hip)
 
-        # 1. get avg if either shoulders or ears have left-right pairs
-        # if (left_shoulder is not None and right_shoulder is not None):
-            # average left and right x and y
-            # self.avg_shoulder = [
-                # (left_shoulder[0] + right_shoulder[0])/2,
-                # (left_shoulder[1] + right_shoulder[1])/2
-            # ]
-            # print("avg shoulder", self.avg_shoulder)
-        # if (left_ear is not None and right_ear is not None):
-            # self.avg_ear = [
-                # (left_ear[0] + right_ear[0])/2,
-                # (left_ear[1] + right_ear[1])/2
-            # ]
-            # print("avg ear:", self.avg_ear)
-
-        # 2. set ear and shoulder using either avg or left or right
-        # if self.avg_shoulder is not None:
-            # shoulder = self.avg_shoulder
-        # else:
-            # try:
-                # print("left shoulder", left_shoulder)
-                # shoulder = [left_shoulder[0], left_shoulder[1]]
-            # except:
-                # print("right shoulder", right_shoulder)
-                # shoulder = [right_shoulder[0], right_shoulder[1]]
-        # if self.avg_ear is not None:
-            # ear = self.avg_ear
-        # else:
-            # try:
-                # print("left ear", left_ear)
-                # ear = [left_ear[0], left_ear[1]]
-            # except:
-                # print("right ear", right_ear)
-                # ear = [right_ear[0], right_ear[1]]
-        # print("ear vs shoulder", ear, shoulder)
-
-        # A3. compare ear and shoulder
-        # if (ear is not None and shoulder is not None):
-            # if ear[0] > (shoulder[0] - self.offset):
-                # self.ear_before_shoulder = True
-                # self.posture = "Bad :("
-            # else:
-                # self.ear_before_shoulder = False
-                # self.posture = "Good :)"
-
-        # A3. compare hip and shoulder
+        # compare shoulder and hip
         if (hip is not None and shoulder is not None):
             if DIRECTION == "right":
                 if shoulder[0] > (hip[0] - self.offset):
